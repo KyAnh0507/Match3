62de060cc9f017672606d09f60f18c5cdaae9075
KyAnh0507 <anhbk@abigames.com.vn>
2024-12-17 18:01:43 +0700

    new

---
+++ Assets/_Game/Scripts/UI/FormHome/LevelProgress/LevelProgressInHome.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgressInHome : MonoBehaviour
+{
+    public List<UILevelProgressInHome> uiLevelProgressInHomes;
+
+    public GameObject prefabUIProgress;
+    private void OnEnable()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/_Game/Scripts/UI/FormHome/LevelProgress/UILevelProgressInHome.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevelProgressInHome : MonoBehaviour
+{
+    public RectTransform tf;
+    public GameObject levelFinished;
+    public GameObject currentLevel;
+    public GameObject levelUnFinish;
+
+    public GameObject fill;
+    public Text indexLevel;
+}
