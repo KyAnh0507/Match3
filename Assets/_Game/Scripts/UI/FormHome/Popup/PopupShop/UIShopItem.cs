65cb976721a4d6550e1543ee9de5c6e55698febd
KyAnh0507 <anhbk@abigames.com.vn>
2024-12-19 18:00:34 +0700

    new

--- Assets/_Game/Scripts/UI/FormHome/Popup/PopupShop/UIShopItem.cs
+++ Assets/_Game/Scripts/UI/FormHome/Popup/PopupShop/UIShopItem.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class UIShopItem : MonoBehaviour
 {
-    public RewardType typeItem;
-    public int amount;
+    public RewardType typeBuy;
+    public RewardType typeReceive;
+
+    public int price;
+    public int quantity;
     // Start is called before the first frame update
     void Start()
     {
