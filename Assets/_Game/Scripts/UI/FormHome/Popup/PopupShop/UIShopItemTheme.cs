d0ea8c0b5c2abb0305979478a98443309bb012e2
KyAnh0507 <kyanh0507@gmail.com>
2025-01-02 22:21:08 +0700

    new

---
+++ Assets/_Game/Scripts/UI/FormHome/Popup/PopupShop/UIShopItemTheme.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShopItemTheme : MonoBehaviour
+{
+    public RewardType currencyType;
+
+    public int price;
+    public int nTheme;
+
+
+}
