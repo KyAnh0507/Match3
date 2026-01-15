a0968de7f42e25065db50eee26860e2781606e3d
KyAnh0507 <kyanh0507@gmail.com>
2026-01-15 14:44:37 +0700

    .

--- Assets/_Game/Scripts/GamePlay/QueueTile.cs
+++ Assets/_Game/Scripts/GamePlay/QueueTile.cs
@@ -43,6 +43,8 @@ public class QueueTile : MonoBehaviour
         }
 
         max = size;
+
+        LevelManager.Ins.currentLevel.canLose = true;
     }
 
     // Update is called once per frame
@@ -105,60 +107,6 @@ public class QueueTile : MonoBehaviour
         
     }
 
-    public void AddScrew(Screw screw)
-    {
-        UndoManager.Ins.AddDataUndo(screw);
-        if (numberScrew < numberTile)
-        {
-            screw.canPlay = false;
-            screw.collider2D.isTrigger = true;
-            LevelManager.Ins.currentLevel.IronRemoveScrew(screw);
-            for (int i = 0; i < numberScrew; i++)
-            {
-                if (!hasScrewType[screw.screwType]) break;
-                if (tile_screws[i].screw.screwType == screw.screwType)
-                {
-                    if (i + 1 < numberScrew && tile_screws[i + 1].screw.screwType == screw.screwType)
-                    {
-                        for (int j = numberScrew; j > i + 2; j--)
-                        {
-                            tile_screws[j].screw = tile_screws[j - 1].screw;
-                            tile_screws[j].screw.Move1(tile_screws[j].tile.TF.position);
-                        }
-                        numberScrew++;
-                        tile_screws[i + 2].screw = screw;
-                        screw.Move(tile_screws[i + 2].tile.TF.position);
-                        StartCoroutine(Match3());
-                        return;
-                    }
-                    else if (i + 1 == numberScrew)
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        for (int j = numberScrew; j > i + 1; j--)
-                        {
-                            tile_screws[j].screw = tile_screws[j- 1].screw;
-                            tile_screws[j].screw.Move1(tile_screws[j].tile.TF.position);
-
-                        }
-                        numberScrew++;
-                        screw.Move(tile_screws[i + 1].tile.TF.position);
-                        tile_screws[i + 1].screw = screw;
-                        return;
-                    }
-                }
-            }
-            hasScrewType[screw.screwType] = true;
-            tile_screws[numberScrew].screw = screw;
-            screw.Move(tile_screws[numberScrew].tile.TF.position);
-            numberScrew++;
-
-            SoundManager.Ins.ChangeSound(SoundType.SCREW_INSERT);
-        }
-    }
-
     public bool IsFull()
     {
         int total = 0;
@@ -169,60 +117,6 @@ public class QueueTile : MonoBehaviour
         return total >= max;
     }
 
-    public IEnumerator Match3()
-    {
-        yield return new WaitUntil(() => !isMatching);
-        isMatching = true;
-        int i;
-        LevelManager.Ins.currentLevel.canWin = false;
-        for (i = 0; i < numberScrew-2; i++)
-        {
-            if (tile_screws[i].screw.screwType == tile_screws[i+1].screw.screwType && tile_screws[i+1].screw.screwType == tile_screws[i+2].screw.screwType)
-            {
-                tile_screws[i].screw.Match3();
-                tile_screws[i+1].screw.Match3();
-                tile_screws[i+2].screw.Match3();
-                
-                break;
-            }
-        }
-        yield return new WaitForSeconds(1f);
-        yield return new WaitForEndOfFrame();
-        Debug.Log("numberScrew: "+ numberScrew + "......" + "i: " + i);
-        for (int j = i; j < numberScrew - 2; j++)
-        {
-            if (tile_screws[j].screw.screwType == tile_screws[j + 1].screw.screwType && tile_screws[j + 1].screw.screwType == tile_screws[j + 2].screw.screwType)
-            {
-                i = j;
-                break;
-            }
-        }
-        if (i + 3 != numberScrew)
-        {
-            for (int j = i; j < numberScrew; j++)
-            {
-                if (j < numberScrew - 3)
-                {
-                    tile_screws[j].screw = tile_screws[j + 3].screw;
-                    tile_screws[j].screw.Move1(tile_screws[j].tile.TF.position);
-
-                }
-                else if (j >= numberScrew - 3)
-                {
-                    tile_screws[j].screw = null;
-                }
-            }
-        }
-        numberScrew -= 3;
-        yield return new WaitForSeconds(0.2f);
-        yield return new WaitForEndOfFrame();
-        LevelManager.Ins.currentLevel.numbermatched++;
-        LevelManager.Ins.currentLevel.canWin = true;
-        isMatching = false;
-
-
-        UIManager.Ins.formGame.boosterUndo.ActiceBoosster(numberScrew > 0, DataManager.Ins.dataSaved.boosterUndo);
-    }
     public void Add1Tile()
     {
         numberTile++;
@@ -361,6 +255,7 @@ public class QueueTile : MonoBehaviour
     private IEnumerator MatchRoutine(Match match)
     {
         LevelManager.Ins.currentLevel.canWin = false;
+        LevelManager.Ins.currentLevel.canLose = false;
         yield return new WaitUntil(() => match.IsMatchable());
         yield return new WaitForSeconds(0.1f);
         yield return new WaitUntil(() => match.IsMatchable());
@@ -368,13 +263,14 @@ public class QueueTile : MonoBehaviour
         matches.Remove(match);
         LevelManager.Ins.currentLevel.numbermatched++;
         LevelManager.Ins.currentLevel.canWin = true;
+        LevelManager.Ins.currentLevel.canLose = true;
         yield return new WaitForSeconds(0.4f);
         Reorder();
     }
 
     private IEnumerator LoseRoutine(List<Screw> screws)
     {
-        yield return new WaitUntil(() => IsStable(screws));
+        yield return new WaitUntil(() => IsStable(screws) && LevelManager.Ins.currentLevel.canLose);
         yield return new WaitForSeconds(0.5f);
         if (!IsFull())
         {
